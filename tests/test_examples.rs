6f9fc3760a4520efb38b52d45e3aee8f5794f2d0
Jonathan Leeming <jonathan@leeming.dev>
2023-09-03 13:38:01 +0100

    refactor test_examples to create test per example

--- tests/test_examples.rs
+++ tests/test_examples.rs
@@ -1,105 +1,114 @@
 use std::{
-    collections::HashMap,
     fs::create_dir,
     path::PathBuf,
     process::Command,
 };
 
 
-#[test]
-fn valid_examples() {
-    let examples = HashMap::from([
-        ("empty_program.be", 0),
-        ("exit_failure.be", 20),
-        ("exit_simple.be", 0),
-        ("exit_variable.be", 20),
-        ("let_simple.be", 0),
-        ("let_variable_value.be", 10),
-        ("maths_add_simple.be", 3),
-        ("maths_add_variables.be", 6),
-        ("maths_div_remainder.be", 5),
-        ("maths_div_simple.be", 5),
-        ("maths_mod_simple.be", 0),
-        ("maths_mul_simple.be", 6),
-        ("maths_mul_variables.be", 8),
-        ("maths_sub_simple.be", 1),
-        ("maths_sub_variables.be", 2),
-        ("if_simple_true.be", 1),
-        ("if_simple_false.be", 0),
-        ("if_else_true.be", 0),
-        ("if_else_false.be", 1),
-        ("variable_scoping.be", 5),
-        ("variable_scoping_multiple.be", 3),
-        ("block.be", 3),
-        ("block_if.be", 3),
-        ("comparison_equality_true.be", 0),
-        ("comparison_equality_false.be", 1),
-        ("comparison_nonequality_true.be", 0),
-        ("comparison_nonequality_false.be", 1),
-        ("comparison_lesser_true.be", 0),
-        ("comparison_lesser_false_eq.be", 1),
-        ("comparison_lesser_false.be", 1),
-        ("comparison_lesser_equal_true.be", 0),
-        ("comparison_lesser_equal_true_eq.be", 0),
-        ("comparison_lesser_equal_false.be", 1),
-        ("comparison_greater_true.be", 0),
-        ("comparison_greater_false_eq.be", 1),
-        ("comparison_greater_false.be", 1),
-        ("comparison_greater_equal_true.be", 0),
-        ("comparison_greater_equal_true_eq.be", 0),
-        ("comparison_greater_equal_false.be", 1),
-        ("variable_mutability_valid.be", 1),
-    ]);
-    let examples_dir = PathBuf::from("examples");
-    let build_dir = PathBuf::from("examples/build");
-    if !build_dir.exists() {
-        create_dir(&build_dir).expect("failed to create build dir");
-    }
+macro_rules! valid_example {
+    ($name:ident, $exit_code:tt) => {
+        #[test]
+        fn $name() {
+            let examples_dir = PathBuf::from("examples");
+            let build_dir = PathBuf::from("examples/build");
+            if !build_dir.exists() {
+                create_dir(&build_dir).expect("failed to create build dir");
+            }
 
-    for (example, expected_code) in examples.iter() {
-        let example_file = examples_dir.join(example);
-        let target_file = build_dir.join(example);
-        println!("{example}");
-        assert!(example_file.exists());
-        let compile_args = beryllium::CompileArgs {
-            source_file: example_file,
-            target_file: Some(target_file.clone()),
-        };
-        let compile_result = beryllium::compile(&compile_args);
-        println!("        {compile_result:?}");
-        assert!(compile_result.is_ok());
-        println!("    runnning");
-        let output = Command::new(target_file).output().expect("executable runs correctly");
-        let code = output.status.code();
-        assert!(code.is_some());
-        assert_eq!(code.unwrap(), *expected_code);
-        println!("    SUCCESS\n");
-    }
-}
-
-
-#[test]
-fn invalid_variable_mutability_fails() {
-    let examples_dir = PathBuf::from("examples");
-    let build_dir = PathBuf::from("examples/build");
-    if !build_dir.exists() {
-        create_dir(&build_dir).expect("failed to create build dir");
-    }
-
-    let example = "variable_mutability_invalid.be";
-    let example_file = examples_dir.join(example);
-    let target_file = build_dir.join(example);
-    println!("{example}");
-    assert!(example_file.exists());
-    let compile_args = beryllium::CompileArgs {
-        source_file: example_file,
-        target_file: Some(target_file),
+            let example = stringify!($name.be);
+            let example_file = examples_dir.join(example);
+            let target_file = build_dir.join(example);
+            println!("{example}");
+            assert!(example_file.exists());
+            let compile_args = beryllium::CompileArgs {
+                source_file: example_file,
+                target_file: Some(target_file.clone()),
+            };
+            let compile_result = beryllium::compile(&compile_args);
+            println!("        {compile_result:?}");
+            assert!(compile_result.is_ok());
+            println!("    runnning");
+            let output = Command::new(target_file).output().expect("executable runs correctly");
+            let code = output.status.code();
+            assert!(code.is_some());
+            assert_eq!(code.unwrap(), $exit_code);
+            println!("    SUCCESS\n");
+        }
     };
-    let compile_result = beryllium::compile(&compile_args);
-    println!("        {compile_result:?}");
-    assert!(compile_result.is_err());
-    let err = compile_result.unwrap_err();
-    assert!(matches!(err, beryllium::CompileError::ChangedImmutableVariable(_)));
-    println!("    SUCCESS\n");
 }
+
+
+macro_rules! invalid_example {
+    ($name:ident, $err:pat) => {
+        #[test]
+        fn invalid_variable_mutability_fails() {
+            let examples_dir = PathBuf::from("examples");
+            let build_dir = PathBuf::from("examples/build");
+            if !build_dir.exists() {
+                create_dir(&build_dir).expect("failed to create build dir");
+            }
+
+            let example = stringify!($name.be);
+            let example_file = examples_dir.join(example);
+            let target_file = build_dir.join(example);
+            println!("{example}");
+            assert!(example_file.exists());
+            let compile_args = beryllium::CompileArgs {
+                source_file: example_file,
+                target_file: Some(target_file),
+            };
+            let compile_result = beryllium::compile(&compile_args);
+            println!("        {compile_result:?}");
+            assert!(compile_result.is_err());
+            let err = compile_result.unwrap_err();
+            assert!(matches!(err, $err));
+            println!("    SUCCESS\n");
+        }       
+    };
+}
+
+
+
+valid_example!(empty_program, 0);
+valid_example!(exit_failure, 20);
+valid_example!(exit_simple, 0);
+valid_example!(exit_variable, 20);
+valid_example!(let_simple, 0);
+valid_example!(let_variable_value, 10);
+valid_example!(maths_add_simple, 3);
+valid_example!(maths_add_variables, 6);
+valid_example!(maths_div_remainder, 5);
+valid_example!(maths_div_simple, 5);
+valid_example!(maths_mod_simple, 0);
+valid_example!(maths_mul_simple, 6);
+valid_example!(maths_mul_variables, 8);
+valid_example!(maths_sub_simple, 1);
+valid_example!(maths_sub_variables, 2);
+valid_example!(if_simple_true, 1);
+valid_example!(if_simple_false, 0);
+valid_example!(if_else_true, 0);
+valid_example!(if_else_false, 1);
+valid_example!(variable_scoping, 5);
+valid_example!(variable_scoping_multiple, 3);
+valid_example!(block, 3);
+valid_example!(block_if, 3);
+valid_example!(comparison_equality_true, 0);
+valid_example!(comparison_equality_false, 1);
+valid_example!(comparison_nonequality_true, 0);
+valid_example!(comparison_nonequality_false, 1);
+valid_example!(comparison_lesser_true, 0);
+valid_example!(comparison_lesser_false_eq, 1);
+valid_example!(comparison_lesser_false, 1);
+valid_example!(comparison_lesser_equal_true, 0);
+valid_example!(comparison_lesser_equal_true_eq, 0);
+valid_example!(comparison_lesser_equal_false, 1);
+valid_example!(comparison_greater_true, 0);
+valid_example!(comparison_greater_false_eq, 1);
+valid_example!(comparison_greater_false, 1);
+valid_example!(comparison_greater_equal_true, 0);
+valid_example!(comparison_greater_equal_true_eq, 0);
+valid_example!(comparison_greater_equal_false, 1);
+valid_example!(variable_mutability_valid, 1);
+
+invalid_example!(variable_mutability_invalid, beryllium::CompileError::ChangedImmutableVariable(_));
 
